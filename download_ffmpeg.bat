4213bd17761efbc6ba000a27883b8ff3188ff4ac
Yonghye Kwon <developer.0hye@gmail.com>
2022-06-11 12:16:15 +0900

    upload download_ffmpeg.bat

---
+++ download_ffmpeg.bat
@@ -0,0 +1,3 @@
+wget https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-n5.0-latest-win64-gpl-shared-5.0.zip
+tar xvf ffmpeg-n5.0-latest-win64-gpl-shared-5.0.zip
+ren ffmpeg-n5.0-latest-win64-gpl-shared-5.0 ffmpeg
